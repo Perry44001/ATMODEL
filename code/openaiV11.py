3e344703300404741903db84dcfe17d8f3bf1f19
Perry44001 <126944363+Perry44001@users.noreply.github.com>
2025-07-17 10:51:58 +0800

    Update openaiV11.py

--- code/openaiV11.py
+++ code/openaiV11.py
@@ -3,7 +3,7 @@ import openai
 # openai.api_base = "https://api.openai.com/v1" # 换成代理，一定要加 v1
 openai.api_base = "https://apikeyplus.com/v1" # 换成代理，一定要加 v1
 # openai.api_key = "API_KEY"
-openai.api_key = "sk-kY0d4xy7dIz52GZY6bD1C6D9A380412398Cb924cCc14Af52"
+openai.api_key = "s**"
 
 for resp in openai.ChatCompletion.create(
                                     model="gpt-4o",
