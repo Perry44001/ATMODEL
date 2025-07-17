edcf19ef1ea7a1495cd251ad2f084727865cbf82
Perry44001 <126944363+Perry44001@users.noreply.github.com>
2025-07-17 10:53:29 +0800

    Update xunfeiMd.py

--- code/xunfeiMd.py
+++ code/xunfeiMd.py
@@ -8,7 +8,7 @@ import sys
 SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
 #星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
 SPARKAI_APP_ID = 'f6b98b6f'
-SPARKAI_API_SECRET = 'NmRhYzU5MjRhYzM4ODgyOGNmYzU5Zjlh'
+SPARKAI_API_SECRET = 'N**'
 SPARKAI_API_KEY = 'd**'
 #星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
 SPARKAI_DOMAIN = 'generalv3.5'
